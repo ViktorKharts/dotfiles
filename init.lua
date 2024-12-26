ec403a644863e07bbf195db1fe1269702be04b9f
viktor.k <viktor.kharts@gmail.com>
2024-12-26 21:19:35 +0100

    update options

--- init.lua
+++ init.lua
@@ -9,6 +9,8 @@ vim.o.clipboard = "unnamedplus"
 
 -- 2. movning around, searching and paterns
 vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true -- override 'ignorecase' if uppercase characters provided in shearch
 
 -- 4. displaying text
 vim.o.scr = 20
@@ -24,7 +26,8 @@ vim.o.cursorline = true
 vim.o.hlsearch = false
 
 -- 6. multiple windows
-vim.o.scl = "yes"
+vim.o.signcolumn = "yes" -- draw signcolumn
+vim.o.laststatus = 0 -- status line for last window
 
 -- 9. using the mouse
 vim.o.mouse = ''
@@ -32,16 +35,28 @@ vim.o.mouse = ''
 -- 10. messages and info
 vim.o.ruler = true
 
+-- 12. editing text
+vim.o.completeopt = "menuone,noselect"
+vim.o.undofile = true -- auto same undo history
+
 -- 13. tabs and indenting
-vim.o.ts = 4 -- tabstop
-vim.o.vts = "4" -- vartabstop
-vim.o.sw = 4 -- shiftwidth
+vim.o.tabstop = 2 -- num of spaces for a tab
+vim.o.vartabstop = "2,2,2,2" -- vartabstop
+vim.o.sw = 2 -- shiftwidth
+vim.o.expandtab = true
+vim.o.smartindent = true -- better indentation
 
 -- global vars 
-vim.o.updatetime = 250
+vim.o.updatetime = 200 -- auto write swap file to disk
 vim.o.timeoutlen = 300
 vim.o.guicursor = ""
-vim.o.termguicolors = false
+vim.o.termguicolors = true
+vim.o.pumblend = 10 -- popup menu transparency
+vim.o.pumheight = 10 -- max items in the menu
+vim.o.sessionoptions = "buffers,tabpages,curdir,winsize" -- mksession items to save
+vim.o.splitkeep = "screen" -- keep the text on the same screen line when window resizing
+vim.o.splitright = true -- always split to the right
+
 
 -- global keymappings
 local keymap = vim.keymap
@@ -74,6 +89,7 @@ local wk = require("which-key")
 wk.add({
 	{ "<C-h>", group = "Harpoon" },
 })
+
 vim.keymap.set("n", "<leader>z", "<cmd>:Lazy<cr>", { desc = "Plugin Manager" })
 
 -- Useful functions
--- lua/plugins/gruvbox-material.lua
+++ lua/plugins/gruvbox-material.lua
@@ -1,12 +1,12 @@
 return {
 	{
 		'sainnhe/gruvbox-material',
-		lazy = false,
+		event = "VeryLazy",
 		priority = 1000,
 		config = function()
 			-- Optionally configure and load the colorscheme
 			-- directly inside the plugin declaration.
-			vim.g.gruvbox_material_enable_italic = true
+			vim.g.gruvbox_material_enable_italic = false
 			vim.g.gruvbox_material_cursor = 'orange'
 			vim.cmd.colorscheme('gruvbox-material')
 		end
