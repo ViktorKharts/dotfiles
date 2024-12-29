e1b67b797092b418f445a3c1f828597b20041a43
viktor.k <viktor.kharts@gmail.com>
2024-12-29 16:07:24 +0100

    minimal refactor;

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,6 +8,10 @@ return {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
   },
+	{
+    "numToStr/Comment.nvim",
+    opts = {},
+  },
   {
     "rcarriga/nvim-notify",
     event = "VeryLazy",
@@ -17,50 +21,21 @@ return {
     },
   },
   {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
     config = true,
   },
   {
-    "NeogitOrg/neogit",
+    "windwp/nvim-ts-autotag",
     event = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",       
-      "sindrets/diffview.nvim",        
-      "nvim-telescope/telescope.nvim",
-    },
-    config = true
   },
   {
-    "airblade/vim-gitgutter",
+    "mbbill/undotree",
     event = "VeryLazy",
-    config = function() 
-      local keymap = vim.keymap.set
-
-      keymap("n", "<C-g>t", "<cmd>GitGutterToggle<cr>", { desc = "Toggle Gitgutter" })
-    end,
-  },
-  {
-    "f-person/git-blame.nvim",
-    opts = {
-      enabled = true,
-      message_template = "• <author> • <date> • <summary> •",
-      date_format = "%m-%d-%Y %H:%M:%S",
-      virtual_text_column = 1, 
-    },
-    keys = {
-      { "<C-g>bt", "<cmd>GitBlameToggle<cr>", desc = "Toggle GitBlame" },
-      { "<C-g>bcc", "<cmd>GitBlameCopyCommitURL<cr>", desc = "Copy Commit URL" },
-      { "<C-g>bcf", "<cmd>GitBlameCopyFileURL<cr>", desc = "Copy File URL" },
-      { "<C-g>bcs", "<cmd>GitBlameCopySHA<cr>", desc = "Copy Commit SHA" },
-    },
-  },
-  {
-    "ruifm/gitlinker.nvim",
-    event = "BufReadPre",
-    opts = {
-      mappings = "<leader>gb",
-    },
   }
 }
 
